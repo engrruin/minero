7d640253d93f18987f2bd1289c5990a2e7bf3a39
engrruin <84214239+engrruin@users.noreply.github.com>
2021-05-15 10:27:59 +0800

    Create bttcoin.sh

---
+++ bttcoin.sh
@@ -0,0 +1,6 @@
+#bttcoin
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz
+tar -zxf xmrig-5.11.3-xenial-x64.tar.gz
+cd xmrig-5.11.3
+./xmrig --donate-level 5 -o rx.unmineable.com:3333 -a rx -k -u BTT:TJsxwdeycRx4sZxXGQxgShFiiBxRSnvsMq.test123
